73b50a3aad4995de90cf8516504ad9310e46e238
agwego <agwego@5d8536f3-8ef1-4ef6-c4bf-857ef619193d>
2010-03-31 04:22:05 +0000

    More unit tests for FuzzTestRunner

--- test/com/agwego/fuzz/FuzzTesterTest.java
+++ test/com/agwego/fuzz/FuzzTesterTest.java
@@ -24,16 +24,27 @@
 
 package com.agwego.fuzz;
 
+import com.agwego.fuzz.annotations.Fuzz;
+import com.agwego.fuzz.annotations.Parameters;
 import com.agwego.fuzz.exception.ParametersError;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 
-import static org.junit.Assert.fail;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Tim Desjardins
@@ -45,7 +56,7 @@ public class FuzzTesterTest
 {
 	protected final Log log = LogFactory.getLog( getClass() );
 
-    @Test
+    @org.junit.Test
     public void parameters()
     {
 	    boolean exceptionCaught = false;
@@ -63,11 +74,35 @@ public class FuzzTesterTest
 	    assertTrue( exceptionCaught );
     }
 
-	@RunWith( FuzzTester.class )
-	@FuzzTester.Parameters( TestDirectory = "test/com/agwego/no_directory", Prefix = "NoFile" )
-	private class TestParameters
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/no_directory", Prefix = "NoFile" )
+			public class TestParameters
+			{
+			}
+
+	@Test
+	public void missingPrefix()
 	{
+		boolean exceptionCaught = false;
+		try {
+		    new FuzzTester( FuzzTesterTest.TestParameters.class );
+		} catch( ParametersError ex ) {
+			log.info( "x Exception: " + ex.getMessage() );
+			fail();
+		} catch( InitializationError ex ) {
+			assertEquals( ex.getCauses().size(), 1 );
+			assertEquals( "The dirName isn't a directory or there was an error reading the directory: test/com/agwego/no_directory", ex.getCauses().get( 0 ).getMessage() );
+			exceptionCaught = true;
+		}
+
+		assertTrue( exceptionCaught );
 	}
+
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/no_directory" )
+			public class TestMissingPrefix
+			{
+			}
 
 	@Test
 	public void noParameters()
@@ -85,30 +120,120 @@ public class FuzzTesterTest
 		assertTrue( exceptionCaught );
 	}
 
-	@RunWith( FuzzTester.class )
-	private class TestNoParameters
-	{
-	}
+			@RunWith( FuzzTester.class )
+			public class TestNoParameters
+			{
+			}
 
 	@Test
-	public void missingTestDirectory()
+	public void singleJsonFile() throws Exception
+	{
+		FuzzTester ft = new FuzzTester( FuzzTesterTest.TestSingleJsonFile.class );
+		List<Runner> children = ft.getChildren();
+		assertEquals( 1, children.size() );
+		FuzzTestRunner runner = (FuzzTestRunner) children.get( 0 );
+		List<FrameworkMethod> fms = runner.getChildren();
+		assertEquals( 1, fms.size() );
+		FuzzTestMethod ftm = (FuzzTestMethod) fms.get( 0 );
+		assertEquals( "mockTest", ftm.getMethod().getName() );
+	}
+
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/fuzz/fuzz_tester_test", Prefix = "FuzzTesterTest_01" )
+			public class TestSingleJsonFile
+			{
+				@Fuzz
+				public void mockTest( final String input, final String expected )
+				{
+					assertEquals( input, expected );
+				}
+			}
+
+	@Test
+	public void multipleJsonFiles() throws Exception
+	{
+		FuzzTester ft = new FuzzTester( FuzzTesterTest.TestMultipleJsonFiles.class );
+		List<Runner> children = ft.getChildren();
+		assertEquals( 2, children.size() );
+
+		FuzzTestRunner runner = (FuzzTestRunner) children.get( 0 );
+		List<FrameworkMethod> fms = runner.getChildren();
+		assertEquals( 1, fms.size() );
+		FuzzTestMethod ftm = (FuzzTestMethod) fms.get( 0 );
+		assertEquals( "noMethodExists", ftm.getMethod().getName() );
+
+		runner = (FuzzTestRunner) children.get( 1 );
+		fms = runner.getChildren();
+		assertEquals( 2, fms.size() );
+		ftm = (FuzzTestMethod) fms.get( 0 );
+		assertEquals( "mockTest", ftm.getMethod().getName() );		
+	}
+
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/fuzz/fuzz_tester_test", Prefix = "FuzzTesterTest" )
+			public class TestMultipleJsonFiles
+			{
+				@Fuzz
+				public void mockTest( final String input, final String expected )
+				{
+					assertEquals( input, expected );
+				}
+
+				@Fuzz
+				public void noMethodExists( final String input, final String expected )
+				{
+					assertEquals( input, expected );
+				}
+			}
+
+	@Test
+	public void noMethodExists() throws Exception
 	{
 		boolean exceptionCaught = false;
+
+		FuzzTester ft = new FuzzTester( FuzzTesterTest.NoMethodExists.class );
+		List<Runner> children = ft.getChildren();
+		assertEquals( 1, children.size() );
+		FuzzTestRunner runner = (FuzzTestRunner) children.get( 0 );
 		try {
-		    new FuzzTester( FuzzTesterTest.TestNoParameters.class );
-		} catch( ParametersError ex ) {
-			assertEquals( "@Parameters( TestDirectory, Prefix, Suffix = '.json', TestDirectoryRootPropertyName = ''", ex.getMessage() );
+			runner.getChildren();
+		} catch( Throwable ex ) {
 			exceptionCaught = true;
-		} catch( InitializationError ex ) {
-			fail();
 		}
 
 		assertTrue( exceptionCaught );
 	}
 
-	@RunWith( FuzzTester.class )
-	@FuzzTester.Parameters( TestDirectory = "test/com/agwego/no_directory" )
-	private class TestMissingTestDirectory
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/fuzz/fuzz_tester_test", Prefix = "FuzzTesterTest_03" )
+			public class NoMethodExists
+			{
+				@Fuzz
+				public void mockTest( final String input, final String expected )
+				{
+					assertEquals( input, expected );
+				}
+			}
+
+	@Test
+	public void testMethod() throws Exception
 	{
+		FuzzTester ft = new FuzzTester( com.agwego.fuzz.FuzzTesterTest.TestMethod.class );
+		List<Runner> children = ft.getChildren();
+		assertEquals( 1, children.size() );
+		FuzzTestRunner ftRunner = (FuzzTestRunner) children.get( 0 );
+		RunNotifier rn = new RunNotifier();
+		ftRunner.run( rn );
 	}
+
+			@RunWith( FuzzTester.class )
+			@Parameters( TestDirectory = "test/com/agwego/fuzz/fuzz_tester_test", Prefix = "FuzzTesterTest_02" )
+			public class TestMethod
+			{
+				@Fuzz
+				public void mockTest( final String input, final String expected )
+				{
+					assertEquals( input, expected + "x" );
+				}
+			}
 }
