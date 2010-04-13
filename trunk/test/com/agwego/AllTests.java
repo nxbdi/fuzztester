e83f925aadf46a7480a5b37f6ac66cbb2d00888c
agwego <agwego@5d8536f3-8ef1-4ef6-c4bf-857ef619193d>
2010-04-13 02:52:14 +0000

    formatting

--- trunk/test/com/agwego/AllTests.java
+++ trunk/test/com/agwego/AllTests.java
@@ -5,5 +5,6 @@ import org.junit.runners.Suite;
 
 @RunWith(value=Suite.class)
 @Suite.SuiteClasses(value={ com.agwego.common.CommonSuite.class, com.agwego.fuzz.FuzzSuite.class })
-public class AllTests {
+public class AllTests
+{
 }
