94765757398a0c776b49922868229fc0ae6e1e15
agwego <agwego@5d8536f3-8ef1-4ef6-c4bf-857ef619193d>
2010-05-04 04:19:20 +0000

    Added Version, cleaned up imports

---
+++ trunk/src/com/agwego/fuzz/Version.java
@@ -0,0 +1,20 @@
+package com.agwego.fuzz;
+
+import java.util.Properties;
+
+public class Version
+{
+	public static void main( String[] args ) throws java.io.IOException
+	{
+		Properties buildTags = new Properties();
+
+		buildTags.load( ClassLoader.getSystemResourceAsStream( "build_tag.properties" ) );
+
+		System.out.println( "Version: " + buildTags.getProperty( "fuzz.version", "not set" ));
+		System.out.println( "Built By: " + buildTags.getProperty( "build.user.name", "not set" ));
+		System.out.println( "Built On: " + buildTags.getProperty( "build.computer", "not set" ));
+		System.out.println( "Build Date: " + buildTags.getProperty( "build.date", "not set" ));
+		System.out.println( "Build Time Stamp: " + buildTags.getProperty( "build.timestamp", "not set" ));
+		System.out.println( "Build revision summary: " + buildTags.getProperty( "build.revision_number", "not set" ));
+	}
+}
